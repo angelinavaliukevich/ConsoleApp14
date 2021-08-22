bb788104c99b7aac34becb64776b838c9a25b538
Asus <Asus@DESKTOP-EURO60B>
2021-08-22 22:57:59 +0300

    Add project files.

---
+++ ConsoleApp14/Complex.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    class Complex
+    {
+         
+        public double r, i;
+        public Complex Sum(Complex a, Complex b)
+        {
+            Complex res = new Complex();
+            res.r = a.r + b.r;
+            res.i = a.i + b.i;
+            return res;
+            Complex Multiplication(Complex a, Complex b)
+            {
+                Complex res = new Complex();
+                res.r = a.r * b.r;
+                res.i = a.i * b.i;
+                return res;
+            }
+
+            Complex Division(Complex a, Complex b)
+            {
+                Complex res = new Complex();
+                double D = b.r * b.r + b.i * b.i;
+                res.r = (a.r * b.r + a.i * b.i) / D;
+                res.i = (a.i * b.r - a.r * b.i) / D;
+                return res;
+
+                void Print(Complex a)
+                {
+                    Console.Write("{0} + i{1}", a.r, a.i);
+                }
+
+                void PrintLine(Complex a)
+                {
+                    Console.WriteLine("{0} + i{1}", a.r, a.i);
+                }
+            }
+        }
+    class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.Write("Введите целую часть первого комплексного числа: ");
+                double r1 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите мнимую часть первого комплексного числа: ");
+                double i1 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите целую часть второго комплексного числа: ");
+                double r2 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите мнимую часть первого комплексного числа: ");
+                double i2 = Convert.ToDouble(Console.ReadLine());
+                Console.Clear();
+                Complex c1 = new Complex();
+                Complex c2 = new Complex();
+                c1.r = r1;
+                c1.i = i1;
+                c2.r = r2;
+                c2.i = i2;
+                Console.WriteLine("Введенные комплексные числа: ");
+                Console.WriteLine("\nДля продолжения нажмите [Enter]");
+                Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine("Сложение комплексных чисел:       {0} + {1}i", c1.Sum(c1, c2).r, c1.Sum(c1, c2).i);
+                Console.WriteLine("Умножение комплексных чисел:      {0} + {1}i");
+                Console.WriteLine("Деление комплексных чисел:      {0} + {1}i" );
+                Console.WriteLine("\nДля выхода из программы нажмите [Enter]");
+                Console.ReadLine();
+            }
+        }
+        }
+    }
+
---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                var myClass = new MyClass();
+                myClass.Change(4, 3);
+                myClass.Print();
+                myClass.Sum();
+                myClass.Max();
+            }
+
+        }
+        private class MyClass
+        {
+            private int x;
+            private int y;
+            public void Change(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+            public void Sum()
+            {
+                Console.WriteLine($"сумма: {x + y}");
+            }
+            public void Max()
+            {
+                Console.WriteLine(x > y ? $"{x} больше чем {y}" : $"{x} меньше чем {y}");
+            }
+            public void Print()
+            {
+                Console.WriteLine($"x: {x} y: {y}");
+            }
+
+
+
+         struct Student
+            {
+                public string Name;
+                public int[] ses;
+                public int Group;
+                public Student(string n, int g, int[] s)
+                {
+                    ses = s;
+                    Name = n;
+                    Group = g;
+                }
+                public int getMiddleScore() //идиотское название
+                {
+                    return (ses[0] + ses[1] + ses[2] + ses[3] + ses[4]) / 5;
+                }
+            }
+            class demo
+            {
+                static void Main()
+                {
+                    string s;
+                    string[] c;
+                    int[] d = new int[5];
+                    bool morefour = false;
+                    Student[] stud1 = new Student[10];
+                    for (int i = 0; i < stud1.Length; i++)
+                    {
+                        Console.Write("Введите имя : ");
+                        stud1[i].Name = Console.ReadLine();
+                        Console.Write("Введите номер группы : ");
+                        stud1[i].Group = Int32.Parse(Console.ReadLine());
+                        Console.Write("Введите оценки : ");
+                        s = Console.ReadLine();
+                        stud1[i].ses = new int[5];
+                        c = s.Split(null);
+                        for (int j = 0; j < 5; j++) stud1[i].ses[j] = Int32.Parse(c[j]);
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine("Студенты со средним баллом больше 4 : ");
+                    foreach (Student st in stud1)
+                    {
+                        if (st.getMiddleScore() >= 4)
+                        {
+                            Console.WriteLine(st.Name);
+                            morefour = true;
+                        }
+                    }
+                    if (!morefour) Console.WriteLine("Нету людей с хорошими оценками!");
+                }
+            }
+        }
+    }
+}
+
+    
